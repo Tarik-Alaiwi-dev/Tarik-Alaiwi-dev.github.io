51c21b91c75cf1b145263c1f8735b54e0df1659f
Tarik-Alaiwi-dev <153948308+Tarik-Alaiwi-dev@users.noreply.github.com>
2023-12-25 17:35:28 +0100

    adding features, fixing bugs

--- weather/scripts.js
+++ weather/scripts.js
@@ -3,6 +3,7 @@ let tempValue = document.querySelector(".temp");
 let humidityValue = document.getElementById("humidity");
 let windValue = document.getElementById("wind");
 let cityName = document.querySelector(".place");
+let image = document.getElementById("image");
 
 let city = "London";
 let apiKey = "8043277f2aa535f922c96a6256c67668";
@@ -13,36 +14,53 @@ let lon = 0;
 let temp = 0;
 let humidity = 0;
 let wind = 0;
+let clouds = 0;
 
-async function checkCity(city) {
-    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`);
-    const cityData = await response.json();
-    console.log(cityData);
-    lat = Number(cityData[0].lat);
-    lon = Number(cityData[0].lon);
-    console.log(lat);
-    console.log(lon);
-}
+// async function checkCity(city) {
+//     const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`);
+//     const cityData = await response.json();
+//     console.log(cityData);
+//     lat = Number(cityData[0].lat);
+//     lon = Number(cityData[0].lon);
+//     console.log(lat);
+//     console.log(lon);
+// }
 
 async function checkWeather() {
-    inputElement = document.getElementById("input").value;
-    city = inputElement;
-    checkCity(city);
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+    inputElement = document.getElementById("input");
+    city = inputElement.value;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`);
     const cityWeather = await response.json();
     console.log(cityWeather);
-    console.log(cityWeather.list[0].main.temp);
-    console.log(cityWeather.list[0].main.humidity + "%");
-    console.log(cityWeather.list[0].wind.speed + "km/h");
-    temp = (cityWeather.list[0].main.temp - 272.15).toFixed(1);
-    humidity = cityWeather.list[0].main.humidity;
-    wind = cityWeather.list[0].wind.speed;
+    console.log(cityWeather.main.temp);
+    console.log(cityWeather.main.humidity + "%");
+    console.log(cityWeather.wind.speed + "km/h");
+    temp = cityWeather.main.temp.toFixed(1);
+    humidity = cityWeather.main.humidity;
+    wind = cityWeather.wind.speed;
+    //check clouds and display proper image
+    clouds = cityWeather.clouds.all;
+    console.log(clouds);
+    if(clouds>=80){
+        image.src = "./images/sun.webp";
+        image.alt = "sun";
+    }else if(clouds<80 && clouds>=60){
+        image.src = "./images/little-cloudy.webp";
+        image.alt = "little";
+    }else if(clouds<60 && clouds>=40){
+        image.src = "./images/very-cloudy.webp";
+        image.alt = "very-cloudy";
+    }else{
+        image.src = "./images/cloudy.webp";
+        image.alt = "cloudy";
+    }
     //assign city weather values
     cityName.innerHTML = city;
     tempValue.innerHTML = temp + "&degC";
     humidityValue.innerHTML = humidity + "%";
     windValue.innerHTML = wind + "km/h";
-    //console.log(temp.toFixed(1));
+    //clear input
+    inputElement.value = "";
 }
 
 checkWeather();
