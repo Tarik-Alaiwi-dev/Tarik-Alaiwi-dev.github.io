982cb224ad9232b7f8677c3638d29aaf0d41c9fe
Tarik Alaiwi <tarik.alaiwi.dev@gmail.com>
2024-10-21 23:11:36 +0200

    fix CV name

--- scripts.js
+++ scripts.js
@@ -55,8 +55,8 @@
 
       function download(){
         const link = document.createElement('a');
-        link.href = 'my_cv/TARIK_ALAIWI_CV.pdf';
-        link.download = 'TARIK_ALAIWI_CV.pdf';
+        link.href = 'my_cv/Tarik_Alaiwi_CV.pdf';
+        link.download = 'Tarik_Alaiwi_CV.pdf';
         link.style.display = 'none';
         document.body.appendChild(link);
         link.click();
